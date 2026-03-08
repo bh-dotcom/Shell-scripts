77d0b4f985ff9fc83a5413ed394f7e9b2a16bf38
Bhoomika M <73037292+bh-dotcom@users.noreply.github.com>
2026-03-08 14:36:44 +0530

    Change author name and clean up comments
    Updated author name in the script header and removed service list comments.

--- aws_resource_list.sh
+++ aws_resource_list.sh
@@ -1,32 +1,11 @@
 #!/bin/bash
 
-# Author: Santhoshkumar
+# Author: Bhoomika
 # Version: v1.0.0
 
 # Script to automate the process of listing all the resources in an AWS account
 # This Script only works in AWS CLI installed and configured machines.
-#
-# Below are the services that are supported by this script:
-# 1. EC2
-# 2. RDS
-# 3. S3
-# 4. CloudFront
-# 5. VPC
-# 6. IAM
-# 7. Route53
-# 8. CloudWatch
-# 9. CloudFormation 
-# 10. Lambda
-# 11. SNS
-# 12. SQS
-# 13. DunamoDB
-# 14. VPC
-# 15. EBS
-#
-# The script will prompt the user to enter the AWS region and the service for which the resource need to be lsited.
-#
-# Usage: ./aws_resource_list.sh <aws_region> <aws_service>
-# Example: ./aws_resource_list.sh us-east-1 ec2
+
 
 if [ $# -ne 2 ]; then
 	echo "Usage: ./aws_resource_list.sh <aws_region> <aws_service>"
